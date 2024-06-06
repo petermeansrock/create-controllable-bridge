89e243ac712421f3173f3f1d40a45579bb3cbf38
Peter VanLund <792688+petermeansrock@users.noreply.github.com>
2024-06-06 22:19:51 +0000

    chore: replace sample code with stub for mod

--- src/main/java/com/petermeansrock/minecraft/create/controllable/CreateControllableBridgeMod.java
+++ src/main/java/com/petermeansrock/minecraft/create/controllable/CreateControllableBridgeMod.java
@@ -1,4 +1,4 @@
-package com.example.modid;
+package com.petermeansrock.minecraft.create.controllable;
 
 import com.simibubi.create.Create;
 
@@ -10,9 +10,9 @@ import net.minecraft.resources.ResourceLocation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ExampleMod implements ModInitializer {
-	public static final String ID = "modid";
-	public static final String NAME = "Example Mod";
+public class CreateControllableBridgeMod implements ModInitializer {
+	public static final String ID = "create-controllable-bridge";
+	public static final String NAME = "Create Controllable Bridge";
 	public static final Logger LOGGER = LoggerFactory.getLogger(NAME);
 
 	@Override
